d3f706b3863e9e8033194dbe9da5aec99ea548d2
sidsoeharto <msidiq.15@gmail.com>
2021-01-20 15:56:35 +0700

    config pm2

---
+++ start.js
@@ -0,0 +1,6 @@
+const exec = require('child_process').exec
+
+exec('cd client && npm start')
+exec('cd server/orchestrator && nodemon index.js')
+exec('cd server/services/movies && nodemon app.js')
+exec('cd server/services/series && nodemon app.js')
